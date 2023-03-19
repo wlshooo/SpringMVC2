f6880974150d0a931069e153b18d5310366e1a90
jinhoLee <kswlsgh@naver.com>
2023-03-19 12:56:12 +0900

    Bean Validation -HTTP 메시지 컨버터

--- validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
+++ validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -1,32 +1,26 @@
-package hello.itemservice.domain.item;
+package hello.itemservice.web.validation.form;
+
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
-import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
-public class Item {
+public class ItemUpdateForm {
 
-
+    @NotNull
     private Long id;
 
-
+    @NotBlank
     private String itemName;
 
+    @Range(min = 1000,max=1000000)
+    @NotNull
     private Integer price;
 
+    //수정에서 수량은 자유롭게 변경가능
     private Integer quantity;
-
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }

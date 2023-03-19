a51bee2e4517c06b0b092d579b14a9da4f6cd62d
jinhoLee <kswlsgh@naver.com>
2023-03-19 23:36:03 +0900

    Filter

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -11,6 +11,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -83,7 +84,7 @@ public class LoginController {
         return "redirect:/";
     }
 
-    @PostMapping("/login")
+    //@PostMapping("/login")
     public String loginV3(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
         if (bindingResult.hasErrors()) {
             return "login/loginForm";
@@ -102,6 +103,30 @@ public class LoginController {
         session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/login")
+    public String loginV4(@Valid @ModelAttribute LoginForm form, BindingResult bindingResult,
+                          HttpServletRequest request, @RequestParam(defaultValue = "/")String redirectURL) {
+
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword()); //폼에서 가져온 정보를 통해 loginService로 확인
+
+        if (loginMember == null) {  //로그인 정보가 null -> 로그인 정보가 맞지 않다.
+            bindingResult.reject("loginFail","아이디 또는 비밀번호가 맞지 않습니다."); //reject를 통해 오류 메시지 출력
+            return "login/loginForm";
+        }
+
+        //로그인 성공 처리
+        //세션이 있으면 있는 세션 반환, 없으면 신규 세션을 생성
+        HttpSession session = request.getSession(true);
+        //세션에 로그인 회원 정보 보관
+        session.setAttribute(SessionConst.LOGIN_MEMBER,loginMember);
+
+        return "redirect:" + redirectURL;
     }
 
     @PostMapping("/logout")
